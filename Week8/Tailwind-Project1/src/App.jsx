14373d4a4591a5894613a28b695d5f002670da8a
Deepak Choure <dchoure2306@gmail.com>
2024-08-01 17:59:34 +0530

    Tailwind basic

---
+++ Week8/Tailwind-Project1/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { RevenueCard } from './components/RevenueCard'
+import './App.css'
+
+function App() {
+  return (
+    <>
+   
+
+
+    </>
+
+
+  )
+
+
+}
+
+export default App
