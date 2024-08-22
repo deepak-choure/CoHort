eccc25c102206f007fd3897024b39277e532d13b
Deepak Choure <dchoure2306@gmail.com>
2024-08-22 19:59:26 +0530

    connectionpooling using accelerate

---
+++ Week12/cnctionPooling/prisma-cloudflare-worker-example/prisma/migrations/20240821163348_user_add_for_pooling/migration.sql
@@ -0,0 +1,58 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TodoUsers" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TodoUsers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "addresses" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "city" VARCHAR(100) NOT NULL,
+    "country" VARCHAR(100) NOT NULL,
+    "street" VARCHAR(255) NOT NULL,
+    "pincode" VARCHAR(20),
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TodoUsers_username_key" ON "TodoUsers"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TodoUsers_email_key" ON "TodoUsers"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "TodoUsers"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "addresses" ADD CONSTRAINT "addresses_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "TodoUsers"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
