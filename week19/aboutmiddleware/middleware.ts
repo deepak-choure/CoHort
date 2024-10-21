a14796eb5e52ea3d34dfedcfbd3da22801032f0a
Deepak Choure <dchoure2306@gmail.com>
2024-10-21 23:49:23 +0530

    middleware in nextjs

---
+++ week19/aboutmiddleware/middleware.ts
@@ -0,0 +1,21 @@
+ import {NextRequest, NextResponse} from "next/server"
+// let requestCount =0;
+// export function middleware(req:NextRequest){
+//     requestCount++;
+//    const res = NextResponse.next()
+//    console.log("Request count",requestCount);
+//    return res;
+// }
+// export const config = {
+//     matcher:"/api/:path*"
+// }
+
+export function middleware(request:NextRequest){
+    console.log(request.nextUrl.pathname);
+    if(request.nextUrl.pathname.startsWith("/admin")){
+        return NextResponse.redirect(new URL("/signin",request.url))
+    }
+    if(request.nextUrl.pathname.startsWith("/dashboard")){
+        return NextResponse.next()
+    }
+}
